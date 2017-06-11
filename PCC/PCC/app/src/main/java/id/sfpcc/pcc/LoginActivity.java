781199289b7a7b60d4999d641f50af0f9918f3b3
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2017-06-11 11:55:03 +0700

    layout

---
+++ PCC/PCC/app/src/main/java/id/sfpcc/pcc/LoginActivity.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017. Gilang Ramadhan (gilangramadhan96.gr@gmail.com)
+ */
+
+package id.sfpcc.pcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
